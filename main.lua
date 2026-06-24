0228cf1f867671bb03217e65380202d656ae5d08
Brynne Alexander <brynne.emaline@gmail.com>
2026-06-24 18:06:43 -0500

    created seperate files for gameloop and randomize functions, added exception handling for invalid inputs

--- main.lua
+++ main.lua
@@ -1,73 +1,35 @@
+dofile("gameLoop.lua")
+
 --[[
 Brynne Alexander
 ~~~
 Lua based Guessing Game
-- The program generates a random number between 1 and 100. (I have left in a print statement to display the generated random number
-    for testing purposes) 
+- The program generates a random number between 1 and 100. (print statement displaying the generated random number is for testing purposes) 
 - The user has 7 guesses to guess the correct number
-- If the user guesses the correct number, they win and are prompted to play again. 
-    They are also prompted to play again if they lose after 7 guesses.
+- If the user guesses the correct number within 7 guesses, they win 
+- If the user doesn't get the correct number within 7 guesses, they lose
+- The user is prompted to play again after both win and lose conditions
 ]]
 
+-- determines if the user wants to play again, ensures game loop will always run at least once
+play = true;
 
+while play do
+    gameLoop();
 
+    print("Play again? (y/n)");
+    answer = io.read();
 
+    -- error handling for invalid inputs
+    while answer ~= "y" and answer ~= "n" do
+        print("Please chose either y or n")
+        answer = io.read();
+    end
 
-
--- logic for random number generation
-function randomize()
-    math.randomseed(os.time()) -- random initialize
-    math.random(); math.random(); math.random() -- warming up generator by discarding first few values
-
-    for x = 1, 20 do -- do this 20 times
-       return math.random(1, 100); -- random generating
+    if answer == "n" then
+        play = false;
     end
 end
 
-print("~~~ Guessing Game ~~~");
+print("Thanks for playing!");
 
-    -- continues prompting for guesses until win / loss condition met
-    -- then prompts to play again, if yes then restarts game loop with new random number and resets guess count, if no then ends program
-    function gameLoop()
-        guessCount = 0;
-        randomNumber = randomize(); -- generate random number for the game loop
-        print("random number generated: " .. randomNumber); -- for testing purposes, can be removed later
-
-    while guessCount < 7 do
-        print("\nGuess a number between 1 and 100");
-        userGuess = tonumber(io.read());
-
-        if userGuess == randomNumber then
-            print("Woohoo, you win! The correct number was " .. randomNumber);
-            guessCount = guessCount + 1; -- not really needed when they win, but in case I want to add functionality later to display how many guesses it took to win
-            print("Start a new game? (y/n)");
-            local playAgain = io.read();
-            if playAgain == "y" then
-                gameLoop(); -- start a new game 
-            else
-                print("Thanks for playing!");
-                break;
-            end
-
-        else
-            if userGuess ~= randomNumber then
-                print("Wrong, try again!");
-                guessCount = guessCount + 1;
-            end
-
-            if guessCount == 7 then
-                print("You lose ): The correct number was " .. randomNumber);
-                print("Start a new game? (y/n)");
-                local playAgain = io.read();
-                if playAgain == "y" then
-                    gameLoop(); -- start a new game 
-                else
-                    print("Thanks for playing!");
-                    break;
-                end
-            end
-        end
-    end
-end
-
-gameLoop(); 
